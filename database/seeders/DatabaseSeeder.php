24e0cfb620cc9bf653d9c0b3b75567bc42d43f16
ryanaidilp <fajrianaidilp@gmail.com>
2021-08-01 22:09:16 +0800

    ref : update to lumen version 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
-class NationalStatSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +13,6 @@ class NationalStatSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // $this->call('UsersTableSeeder');
     }
 }
